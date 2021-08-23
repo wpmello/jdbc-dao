5386bed375d89304936be0837e9f9698062e006c
wpmello <wagnermello903@gmail.com>
2021-08-23 17:59:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-			Department dep = new Department(1, "Books");
-			System.out.println(dep);
+			Department department = new Department(1, "Books");
+			Seller seller = new Seller(2, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+			
+			System.out.println(seller);
 	}
-
 }
