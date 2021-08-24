e49b2ef994b24885affa0450a7dc2e8c1b734796
wpmello <wagnermello903@gmail.com>
2021-08-24 18:16:51 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,18 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		/*
 		System.out.println("\n===== Teste 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inseted! new id = " + newSeller.getId());
+		System.out.println("Inserted! new id = " + newSeller.getId());
+		*/
+		
+		System.out.println("\n===== Teste 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 }
   
