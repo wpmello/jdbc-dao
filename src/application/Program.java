4ec4fcaf8470168cacc22cd84492332a261e0638
wpmello <wagnermello903@gmail.com>
2021-08-24 15:03:31 -0300

    findById implementation and 'Connection' on class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,18 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		// we invoke our Interface and we instanciants it with our 'DaoFactory' and that's why the implementation wont exposed it
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
+		
 	}
 }
   
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	// the way this class was instantiated is to invite implementation will be exposed.
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
