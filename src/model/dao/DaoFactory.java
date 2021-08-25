3d23848a6dc1371c944c2bb6c89545e37e9a2f28
wpmello <wagnermello903@gmail.com>
2021-08-25 11:07:13 -0300

    DepartmentDaoJDBC and DaoFactory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,5 +9,9 @@ public class DaoFactory {
 	// the way this class was instantiated is to invite implementation will be exposed.
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC();
 	}
 }
